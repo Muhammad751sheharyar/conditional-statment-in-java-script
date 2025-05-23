7d0a81ceebb97d37e54866644089f2895507eb54
Muhammad751sheharyar <sheharyar117794@gmail.com>
2025-05-24 00:05:52 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,52 @@
+// var num=prompt("enter the num");
+// if( num >=100 ){
+//     document.write("number is grater equal than 100");
+// }
+// else if(num <100){
+//     document.write("number is less than 100");
+// }
+// else{
+// document.write("num is not define")
+// }
+
+var firstNum=prompt("Enter the first number");
+var secondNum=prompt("Enter the Second number");
+if(firstNum > secondNum){
+    document.write(firstNum);
+}
+else if(secondNum > firstNum){
+document.write(secondNum);
+}else{
+    document.write("pleace enter the number");
+}
+
+
+
+
+// var num=prompt("enter the num");
+// if (num %2==0){
+//        document.write(num+"is dividual 2")
+
+// }
+// else{
+//     document.write(num+"is not divisible 2")
+// }
+
+// var num=prompt("enter the num");
+// if(num%3==0){
+//     document.write(num+"is divisible by 3")
+// }else{
+//     document.write(num+"is not divisible by 3")
+// }
+
+
+// var num=prompt("enter the num");
+// var num1=num%2;
+// document.write(num1);
+
+
+
+
+
+
+
